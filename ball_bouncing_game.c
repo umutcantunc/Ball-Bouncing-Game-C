b48c923e325d439be54894ffa3fa638375d2233c
Umutcan Tunç <umutcantuncc@gmail.com>
2025-12-09 05:47:59 +0300

    Update ball_bouncing_game.c

--- ball_bouncing_game.c
+++ ball_bouncing_game.c
@@ -15,17 +15,17 @@ int main() {
     InitWindow(ekranGenislik, ekranYukseklik, "Ball Bouncing Game");
     SetTargetFPS(60);
 
-    // Fizik Ayarlarý
+    // Fizik Ayarlari
     const float yercekimi = 0.5f;
-    const float ziplamaGucu = 12.0f; // Biraz daha güçlü zýplasýn
+    const float ziplamaGucu = 12.0f; 
 
     int skor = 0;
     bool oyunBitti = false;
 
-    // Top Baþlangýç
+    // Top baslangic
     TopVerisi top;
     top.konum = (Vector2){ ekranGenislik / 2.0f, ekranYukseklik / 2.0f };
-    top.hiz = (Vector2){ 2.0f, 0.0f }; // Hafif bir baþlangýç hýzý
+    top.hiz = (Vector2){ 2.0f, 0.0f }; // baslangicta ufak bir hiz
     top.yaricap = 40.0f;
     top.renk = MAROON;
 
@@ -36,7 +36,7 @@ int main() {
             skor = 0;
             oyunBitti = false;
         }
-        // --- 1. GÜNCELLEME ---
+        
         if (!oyunBitti) {
 
             top.konum.x += top.hiz.x;
@@ -44,49 +44,48 @@ int main() {
             top.hiz.y += yercekimi;
 
 
-            // A) Yerçekimi ve Hareket
-            // Kodunuzu buraya yazýn (Hýzý güncelle, Konumu güncelle):
+            //  Yerï¿½ekimi ve Hareket
+            
 
             if (top.konum.x > 560 || top.konum.x < 40) {
                 top.hiz.x = -top.hiz.x;
             }
 
 
-            // B) Duvarlardan Sektirme (Sað ve Sol)
-            // Kodunuzu buraya yazýn:
+            // Duvarlardan Sektirme (Saï¿½ ve Sol)
+            
 
 
 
 
-            // C) Týklama Kontrolü (Vektörel Fýrlatma)
+            //  Tiklama kontrolu
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
                 CheckCollisionPointCircle(GetMousePosition(), top.konum, top.yaricap)) {
 
-                // 1. Yukarý Fýrlat
+                // 1. Yukari firlat
                 top.hiz.y = -ziplamaGucu;
-                // 2. Yana Fýrlat (YENÝ GÖREV)
-                // Topun merkezi ile fare arasýndaki farký hýza dönüþtürün.
-                // Kodunuzu buraya yazýn:
+                // 2. Yana Firlat 
+                
                 top.hiz.x = (top.konum.x - GetMouseX()) * 0.2f;
 
                 skor++;
             }
 
-            // D) Yere Düþme Kontrolü
+            // Yere dusme kontrol
             if (top.konum.y > ekranYukseklik + top.yaricap) {
                 oyunBitti = true;
             }
         }
 
-        // --- 2. ÇÝZÝM ---
+        // Cizim
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
             if (!oyunBitti) {
-                // Topu Çiz
+                // Topu ciz
                 DrawCircleV(top.konum, top.yaricap, top.renk);
 
-                // Görsel Ýpucu: Topun merkezini göster (Küçük nokta)
+                // Gorsel ipucu: Topun merkezini goster (Kucuk nokta)
                 DrawCircleV(top.konum, 3, WHITE);
 
                 DrawText(TextFormat("Score: %d", skor), 10, 10, 30, LIGHTGRAY);
